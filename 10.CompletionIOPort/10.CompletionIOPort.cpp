386338cbf1f4208bdeb5fa154b78e97ae16daeee
= <=>
2022-09-03 23:37:04 +0800

    ...

--- 10.CompletionIOPort/10.CompletionIOPort.cpp
+++ 10.CompletionIOPort/10.CompletionIOPort.cpp
@@ -1,5 +1,34 @@
-﻿
+﻿#include <stdio.h>
+#include <Windows.h>
+
+
+#define CREATE_NEW_COMPLETION_PORT(numThreads) \
+    CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, numThreads);
+
+#define ASSOCIATE_DEVICE_WITH_COMPLETION_PORT(hCompletionPort, hDevice, dwCompletionKey) \
+    CreateIoCompletionPort(hDevice, hCompletionPort, dwCompletionKey, 0);
+
+
+const int NUM_THREAD = 4;
+const int READ_KEY = 1234;
+const int WRITE_KEY = 4321;
 
 int main() {
-    
+    LPCWSTR pszFilename = L"./test.txt";
+    DWORD ret;
+    DWORD dwSt;
+    BYTE readBuf[256] = { 0 };
+
+    HANDLE hFile = CreateFile(pszFilename, GENERIC_WRITE, FILE_SHARE_WRITE, 0,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE, 0);
+
+    HANDLE hIO = CREATE_NEW_COMPLETION_PORT(NUM_THREAD);
+    HANDLE hAssIO = ASSOCIATE_DEVICE_WITH_COMPLETION_PORT(hIO, hFile, READ_KEY);
+
+    if (hAssIO != hIO) {
+        printf("Init Completion IO failed\n");
+        return -1;
+    }
+
+    int a = 0;
 }
--- DuplicateHandle2/DuplicateHandle2.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <Windows.h>
-#include <stdio.h>
-
-int main() {
-	HANDLE h = CreateMutex(NULL, FALSE, L"TonyMutex");
-	printf("mutex handle is: %lld\n", (DWORD)h);
-
-	while (1);
-}
