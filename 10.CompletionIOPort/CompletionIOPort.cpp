abd5db0d9e2a5e043c52490bc278bcf26b76bab8
= <=>
2022-09-04 20:02:20 +0800

    add set file completion port notification mode

--- 10.CompletionIOPort/CompletionIOPort.cpp
+++ 10.CompletionIOPort/CompletionIOPort.cpp
@@ -12,6 +12,7 @@ ULONG_PTR READ_KEY = 1234;
 ULONG_PTR WRITE_KEY = 4321;
 
 void TestSimpleQueueCompletionPort(int isQueue = 1);
+void TestQueueCompletionPort();
 
 int main() {
     printf("TestSimpleQueueCompletionPort\n");
@@ -20,6 +21,10 @@ int main() {
     printf("\n");
     printf("TestSimpleNotQueueCompletionPort\n");
     TestSimpleQueueCompletionPort(0);
+
+    printf("\n");
+    printf("TestQueueCompletionPort\n");
+    TestQueueCompletionPort();
 }
 
 void TestSimpleQueueCompletionPort(int isQueue) {
@@ -70,6 +75,38 @@ void TestSimpleQueueCompletionPort(int isQueue) {
     }
 
     CloseHandle(ovRead.hEvent);
+    CloseHandle(hIO);
+    CloseHandle(hFile);
+}
+
+void TestQueueCompletionPort() {
+    OVERLAPPED ovRead = { 0 };
+    LPCWSTR pszFilename = L"./CompletionIOPort.cpp";
+    DWORD dw, ret, dwSt;
+    BYTE readBuf[256] = { 0 };
+    OVERLAPPED* pOverlapped;
+
+    HANDLE hFile = CreateFile(pszFilename, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0);
+
+    HANDLE hIO = CREATE_NEW_COMPLETION_PORT(NUM_THREAD);
+    HANDLE hAssIO = ASSOCIATE_DEVICE_WITH_COMPLETION_PORT(hFile, hIO, READ_KEY);
+
+    if (hAssIO != hIO) {
+        printf("Init Completion IO failed\n");
+        return;
+    }
+
+    SetFileCompletionNotificationModes(hFile, FILE_SKIP_COMPLETION_PORT_ON_SUCCESS);
+    ReadFile(hFile, readBuf, 10, NULL, &ovRead);
+    ret = GetQueuedCompletionStatus(hIO, &dw, &READ_KEY, &pOverlapped, 1000);
+    dwSt = GetLastError();
+    if (ret == 0 && dwSt == WAIT_TIMEOUT) {
+        printf("Get queue timeout, set FILE_SKIP_COMPLETION_PORT_ON_SUCCESS successfully\n");
+    }
+    else {
+        printf("Set FILE_SKIP_COMPLETION_PORT_ON_SUCCESS failed\n");
+    }
+
     CloseHandle(hIO);
     CloseHandle(hFile);
 }
