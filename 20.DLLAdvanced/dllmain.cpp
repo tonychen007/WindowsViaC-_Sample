bf49f5f4f9a2fe7730a0f3a87b93916462885879
= <=>
2022-09-08 16:44:15 +0800

    add freeLibraryExitThread

---
+++ 20.DLLAdvanced/dllmain.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+HMODULE g_hDll = NULL;
+
+DWORD WINAPI FreeSelfProc(PVOID param) {
+    MessageBox(NULL, L"Press OK to unload inside DLL", NULL, MB_OK);
+
+    // after call to FreeLibrary, the process will crash
+    // because the EIP no long existed
+    //FreeLibrary(g_hDll);
+    FreeLibraryAndExitThread(g_hDll, 0);
+
+    return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH: {
+        g_hDll = (HMODULE)hModule;
+        HANDLE hThread = ::CreateThread(NULL, 0, FreeSelfProc, NULL, 0, NULL);
+        CloseHandle(hThread);
+    }
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ 20.DLLAdvancedExe/20.DLLAdvancedExe.cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <thread>
+using namespace std;
+
+typedef int (*addFunpadd)(int a, int b);
+LONG MyExceptionHandler(EXCEPTION_POINTERS* excpInfo) {
+	excpInfo->ContextRecord;
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void TestLoadDLLWithFlag(DWORD flag = 0);
+void TestFreeLibraryAndExitThread();
+
+int main() {
+	printf("TestLoadDLLNoRef\n");
+	//TestLoadDLLWithFlag(DONT_RESOLVE_DLL_REFERENCES);
+
+	printf("\n");
+	printf("TestLoadDLLAsDATA\n");
+	//TestLoadDLLWithFlag(LOAD_LIBRARY_AS_DATAFILE);
+
+	printf("\n");
+	printf("TestFreeLibraryAndExitThread\n");
+	TestFreeLibraryAndExitThread();
+}
+
+void TestLoadDLLWithFlag(DWORD flag) {
+	HMODULE hDLL = LoadLibraryEx(L"19.DLLBasic.dll", NULL, flag);
+
+	addFunpadd add = (addFunpadd)GetProcAddress(hDLL, "gVal");
+	__try {
+		add(1, 2);
+	}
+	__except (MyExceptionHandler(GetExceptionInformation())) {
+		printf("GetProcAddress is OK, but the call of add should fail\n");
+	}
+
+	int a = 0;
+}
+
+void TestFreeLibraryAndExitThread() {
+	HMODULE hDLL = LoadLibrary(L"20.DLLAdvanced.dll");
+
+	printf("Press key to end...\n");
+	Sleep(1000);
+	printf("After free inside DLL, please watch the hDLL address via breakpoint\n");
+}
